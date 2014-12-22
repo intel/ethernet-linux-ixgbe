de032de4604802c2dec0f10c9bae74964b278400
Jesse Brandeburg <jesse.brandeburg@intel.com>
2014-12-22 13:27:46 -0800

    Intel official release 3.23.2

--- src/ixgbe.h
+++ src/ixgbe.h
@@ -43,7 +43,6 @@
 #endif
 /* Can't use IS_ENABLED until after kcompat is loaded */
 #if defined(CONFIG_DCA) || defined(CONFIG_DCA_MODULE)
-#define IXGBE_DCA
 #include <linux/dca.h>
 #endif
 #include "ixgbe_dcb.h"
